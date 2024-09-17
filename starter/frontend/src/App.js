9db33438667b24a172f7ba55839923dd7db7bf54
tinhnt <tinhnt>
2024-09-18 00:08:13 +0700

    fix fe ci

--- starter/frontend/src/App.js
+++ starter/frontend/src/App.js
@@ -1,9 +1,10 @@
+import React from 'react';
 import './App.css';
 
 export default function App() {
   return (
     <div className="container">
-      movies cicd project
+      <h1>Movie List</h1>
     </div>
   );
 }
